3ae9e62af795d1861e2a956fd5f1cdfe84bcfb7f
Mateusz Kurek <m.kurek1994@gmail.com>
2026-03-12 15:25:03 +0100

    refactor: streamline socket initialization and update build script in package.json

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,8 @@ app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../web/dist")));
-  app.get("/{*any}", (_req, res) => {
+
+  app.get("/{*any}", (_, res) => {
     res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
   });
 }
